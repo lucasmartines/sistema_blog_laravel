88d3b62304f421611e8b2324fe354a09ff9e6402
Lucas Martines <lucas.alcantarilla97@gmail.com>
2019-10-13 22:15:59 -0300

    primeiro commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app') 
+
+@section("content")
+ <div class="container">
+     <div class="row">
+        <div class="col-4  mx-auto   ">
+            <h2 class="text-center mt-4">
+                Learning laravel 5
+            </h2>
+            <h4 class="text-center">
+                Criação Pratica do blog
+            </h4>
+        </div>
+     </div>
+     <div class="row">
+         <img class="mx-auto" src="https://learninglaravel.net/img/LearningLaravel5_cover6.png" 
+            style="max-width:300px"alt="">
+     </div>
+     <div class="row">
+        <h2 class="text-center w-100">Aplicação criada por Lucas Martines</h2>
+        <h4 class="text-center w-100"> Adaptado do livro Learnign laravel criado por Nathan Wu</h4>
+     </div>
+ </div> 
+  @endsection()
