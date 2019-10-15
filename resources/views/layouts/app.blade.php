d23fb32e23412bfbdfd4a3d1ddb40deda8e48f64
Lucas Martines <lucas.alcantarilla97@gmail.com>
2019-10-15 01:30:10 -0300

    diversas atualizações

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -44,25 +44,7 @@
                             <a href="{{route('dashboard')}}" class="nav-link">
                                 Admin
                             </a>
-                            <li class="nav-item">
-                                <li class="nav-item dropdown">
-                                        <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Roles</a>
-                                    <div class="dropdown-menu" x-placement="bottom-start" style="position: absolute; transform: translate3d(0px, 40px, 0px); top: 0px; left: 0px; will-change: transform;">
-                                        <a class="dropdown-item" href="/roles">Ver Roles</a>
-                                        <a class="dropdown-item" href="/roles/create">Criar</a>
-                                    </div>
-                                </li>
-                            </li>
-                            <li class="nav-item">
-                                <li class="nav-item">
-                                    <li class="nav-item dropdown">
-                                            <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Usuarios</a>
-                                        <div class="dropdown-menu" x-placement="bottom-start" style="position: absolute; transform: translate3d(0px, 40px, 0px); top: 0px; left: 0px; will-change: transform;">
-                                            <a class="dropdown-item" href="/users">Ver Usuarios</a>
-                                        </div>
-                                    </li>
-                                </li>
-                            </li>
+                           
                         @endif
                     @endif
                         
